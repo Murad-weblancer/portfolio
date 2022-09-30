ca01fadd37b863da1de2805bfeccc1718b8ee11b
Murad <kingjalalov23@gmail.com>
2022-09-30 17:30:00 +0300

    last- commisdsdt

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -93,11 +93,11 @@ const Work = () => {
               </div>
             </div>
           </div>
-          <div
+          {/* <div
             style={{ backgroundImage: `url(${realEstate})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
-            {/* Hover Effects */}
+
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
                 React JS Application
@@ -115,7 +115,7 @@ const Work = () => {
                 </a>
               </div>
             </div>
-          </div>
+          </div> */}
           <div
             style={{ backgroundImage: `url(${wt})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
